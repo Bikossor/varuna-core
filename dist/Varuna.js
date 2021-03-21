78398acd27c2e200e3684870cb01e33735f25436
Bikossor <andre.lichtenthaeler@gmail.com>
2021-03-21 19:56:42 +0100

    Closes #7

--- dist/Varuna.js
+++ dist/Varuna.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Varuna = void 0;
 var Fetcher_1 = require("./Fetcher");
 var Varuna = /** @class */ (function () {
     function Varuna(path) {
